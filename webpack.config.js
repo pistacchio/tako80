def5ffaa0d3e47b94c5523e684183a56fb453cee
pistacchio <pistacchio@gmail.com>
2017-06-28 20:46:04 +0200

    + webpack build

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entry: {
+        'tako80.core.js': './src/tako80.core.core.js',
+    },
+    output: {
+        path: __dirname,
+        filename: './src/[name]'
+    }
+};
